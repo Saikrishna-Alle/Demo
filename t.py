490aad6e08b8e00349daef2d78d9eed5ba30061d
Krishna <saikrishnastilk@gmail.com>
2023-11-15 17:33:41 +0530

    multiply Function: 3

--- t.py
+++ t.py
@@ -1,8 +1,15 @@
 x = int(input("Enter a number: "))
 y = int(input("Enter another number: "))
+
 def add(x, y):
     return x + y
+
 def subtract(x, y):
     return x - y
+
+def multiply(x, y):
+    return x * y
+
 print(subtract(x, y))
 print(add(x, y))
+print(multiply(x, y))
