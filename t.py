7bb836b722a9767d27c504d40da6a93dbb817a7c
Krishna <saikrishnastilk@gmail.com>
2023-11-15 15:46:17 +0530

    added Subtract Function: 2

--- t.py
+++ t.py
@@ -2,4 +2,7 @@ x = int(input("Enter a number: "))
 y = int(input("Enter another number: "))
 def add(x, y):
     return x + y
+def subtract(x, y):
+    return x - y
+print(subtract(x, y))
 print(add(x, y))
